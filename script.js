f976ace4f54c625835fe426cb2c7f0150766eede
Aditya Mishra <mishraanshu1735@gmail.com>
2026-05-16 22:33:00 +0530

    Day 3: redesigned portfolio with modern responsive UI

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Portfolio Website Loaded Successfully");
+console.log("Modern Portfolio Loaded 🚀");
+
+/* Simple Fade Animation */
+
+const cards = document.querySelectorAll(
+    ".skill-card, .project-card"
+);
+
+cards.forEach((card) => {
+
+    card.addEventListener("mouseenter", () => {
+        card.style.transform = "translateY(-10px)";
+    });
+
+    card.addEventListener("mouseleave", () => {
+        card.style.transform = "translateY(0)";
+    });
+
+});
