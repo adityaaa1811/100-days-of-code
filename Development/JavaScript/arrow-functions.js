24473b74087c1f9f6725fa9e3878fabecab98e2d
Aditya Mishra <mishraanshu1735@gmail.com>
2026-06-04 22:46:34 +0530

    Day 15: learned arrow functions and forEach

---
+++ Development/JavaScript/arrow-functions.js
@@ -0,0 +1,19 @@
+function greet(name) {
+
+    return `Hello ${name}`;
+}
+
+console.log(
+    greet("Aditya")
+);
+
+/* ARROW FUNCTION */
+
+const greetUser = (name) => {
+
+    return `Hello ${name}`;
+};
+
+console.log(
+    greetUser("Aditya")
+);
