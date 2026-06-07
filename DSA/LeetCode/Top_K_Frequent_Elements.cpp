ed62ee5e7dd763bf81d3486ec5eeb59fd222a47a
Aditya Mishra <mishraanshu1735@gmail.com>
2026-06-07 18:11:58 +0530

    Day 18: solved group anagrams and top k frequent elements

---
+++ DSA/LeetCode/Top_K_Frequent_Elements.cpp
@@ -0,0 +1,33 @@
+#include<vector>
+#include<unordered_map>
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        unordered_map<int,int> mp;
+
+        for(int num : nums){
+            mp[num]++;
+        }
+
+        priority_queue<pair<int,int>> pq;
+
+        for(auto it : mp){
+            pq.push({it.second, it.first});
+        }
+
+        vector<int> ans;
+
+        while(k--){
+
+            ans.push_back(pq.top().second);
+
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
---
+++ Development/JavaScript/sort.js
@@ -0,0 +1,8 @@
+const numbers = [5, 2, 9, 1];
+
+numbers.sort(
+
+    (a, b) => a - b
+);
+
+console.log(numbers);
