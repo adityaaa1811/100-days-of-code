c709a815c453156dd1a113143478a459581162e0
Aditya Mishra <mishraanshu1735@gmail.com>
2026-06-08 18:35:54 +0530

    Day 19: updated portfolio stats and completed array interview patterns

---
+++ Development/JavaScript/array-destructuring.js
@@ -0,0 +1,15 @@
+const skills = [
+
+    "HTML",
+    "CSS",
+    "JavaScript"
+];
+
+const [
+    first,
+    second
+] = skills;
+
+console.log(first);
+
+console.log(second);
---
+++ Development/JavaScript/rest-operator.js
@@ -0,0 +1,16 @@
+const skills = [
+
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React"
+];
+
+const [
+    first,
+    ...remaining
+] = skills;
+
+console.log(first);
+
+console.log(remaining);
